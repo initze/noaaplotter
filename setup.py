48380fc5ff58534cc8d61b7f67f3d2799d264318
Ingmar Nitze <ingmarnitze@gmail.com>
2019-09-02 13:58:20 +0200

    added setup.py fixed matplotlib crash

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='noaaplotter',
-    version='0.1',
-    packages=[''],
+    version='0.1.0',
+    packages=['noaaplotter'],
     url='https://github.com/initze/noaaplotter',
     license='',
     author='initze',
     author_email='ingmar.nitze@awi.de',
-    description='Package to plot fancy climate/weather data',
+    description='Package to plot fancy climate/weather data of NOAA',
     install_requires=['pandas', 'numpy', 'seaborn', 'matplotlib']
 )
