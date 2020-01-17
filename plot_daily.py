68dd6d733faa91000b1590698ff00f71aec51c6b
Ingmar Nitze <ingmarnitze@gmail.com>
2020-01-17 19:37:54 +0100

    added plotting command line script adapted version to 0.1.2 added yml file

---
+++ plot_daily.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from noaaplotter.noaaplotter import NOAAPlotter
+import argparse
+
+def main():
+    """
+    Main Function
+    :return:
+    """
+    ##### Parse arguments #####
+    parser = argparse.ArgumentParser(description='Parse arguments.')
+
+    parser.add_argument('-infile', dest='infile', type=str, required=True,
+                        help='input file with climate data')
+
+    parser.add_argument('-start', dest='start_date', type=str, required=True,
+                        help='start date of plot ("yyyy-mm-dd")')
+
+    parser.add_argument('-end', dest='end_date', type=str, required=True,
+                        help='end date of plot ("yyyy-mm-dd")')
+
+    parser.add_argument('-season', dest='season_mode', nargs=1, required=False,
+                        default='summer', type=str,
+                        help='plot_mode {summer, winter}')
+
+    parser.add_argument('-loc', dest='location', required=False,
+                        type=str, default=None,
+                        help='Location name, must be in data file')
+
+    parser.add_argument('-save_plot', dest='save_path', type=str, required=False,
+                        default=None,
+                        help='filepath for plot')
+
+    parser.add_argument('-p_range', dest='p_range', type=float, required=False,
+                        default='auto',
+                        help='maximum precipitation value in plot')
+
+    parser.add_argument('-t_range', dest='t_range', type=float, nargs=2, required=False,
+                        default=['auto', 'auto'],
+                        help='temperature range in plot')
+
+    parser.add_argument('-dpi', dest='dpi', type=float, required=False,
+                        default=100,
+                        help='dpi for plot output')
+
+    parser.add_argument('-plot', dest='show_plot', required=False,
+                        default=False, action='store_true',
+                        help='Location name, must be in data file')
+
+    args = parser.parse_args()
+
+    ##### Run Plotting function #####
+    n = NOAAPlotter(args.infile,
+                    location=args.location)
+
+    n.plot_weather_series(start_date=args.start_date, end_date=args.end_date,
+    show_snow_accumulation=False, show_plot=args.show_plot, kwargs_fig={'dpi':args.dpi},
+    plot_extrema=True, save_path=args.save_path,
+    plot_tmin=args.t_range[0], plot_tmax=args.t_range[1], plot_pmax=args.p_range)
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='noaaplotter',
-    version='0.1.1',
+    version='0.1.2',
     packages=['noaaplotter'],
     url='https://github.com/initze/noaaplotter',
     license='',
