905606a5720782f31067720ade365af509d4f8a4
Ingmar Nitze <ingmarnitze@gmail.com>
2020-01-18 13:16:27 +0100

    restructured and added snow_acc

--- plot_daily.py
+++ plot_daily.py
@@ -20,10 +20,6 @@ def main():
     parser.add_argument('-end', dest='end_date', type=str, required=True,
                         help='end date of plot ("yyyy-mm-dd")')
 
-    parser.add_argument('-season', dest='season_mode', nargs=1, required=False,
-                        default='summer', type=str,
-                        help='plot_mode {summer, winter}')
-
     parser.add_argument('-loc', dest='location', required=False,
                         type=str, default=None,
                         help='Location name, must be in data file')
@@ -39,6 +35,10 @@ def main():
     parser.add_argument('-t_range', dest='t_range', type=float, nargs=2, required=False,
                         default=[None, None],
                         help='temperature range in plot')
+
+    parser.add_argument('-snow_acc', dest='snow_acc', required=False,
+                        default=False, action='store_true',
+                        help='show snow accumulation, only useful for plotting winter season (e.g. July to June')
 
     parser.add_argument('-dpi', dest='dpi', type=float, required=False,
                         default=100,
@@ -58,11 +58,16 @@ def main():
     n = NOAAPlotter(args.infile,
                     location=args.location)
 
-    n.plot_weather_series(start_date=args.start_date, end_date=args.end_date,
-    show_snow_accumulation=False, show_plot=args.show_plot, kwargs_fig={'dpi':args.dpi},
-    plot_extrema=True, save_path=args.save_path,
-    plot_tmin=args.t_range[0], plot_tmax=args.t_range[1], plot_pmax=args.p_range,
-    figsize=args.figsize)
+    n.plot_weather_series(start_date=args.start_date,
+                          end_date=args.end_date,
+                          show_snow_accumulation=args.snow_acc,
+                          kwargs_fig={'dpi':args.dpi},
+                          plot_extrema=True,
+                          save_path=args.save_path,
+                          plot_tmin=args.t_range[0],
+                          plot_tmax=args.t_range[1],
+                          plot_pmax=args.p_range,
+                          figsize=args.figsize)
 
 if __name__ == "__main__":
     main()
