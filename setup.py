d6102d74bdf42c950d7af62ada0783afae7074a0
initze <ingmarnitze@gmail.com>
2021-09-19 17:34:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from distutils.core import setup
 
 setup(
     name='noaaplotter',
-    version='0.2.0dev',
+    version='0.4',
     packages=['noaaplotter'],
     url='https://github.com/initze/noaaplotter',
     license='',
     author='Ingmar Nitze',
     author_email='ingmar.nitze@awi.de',
     description='Package to plot fancy climate/weather data of NOAA',
-    install_requires=['pandas>=0.25.3', 'numpy>=1.17.4', 'matplotlib>=3.1.1', 
-                      'requests'],
-    scripts=['plot_daily.py', 'plot_monthly.py']
+    install_requires=['pandas', 
+                      'numpy', 
+                      'matplotlib', 
+                      'requests',
+                      'joblib',
+                      'tqdm'],
+    scripts=['plot_daily.py', 'plot_monthly.py', 'download_data.py']
 )
