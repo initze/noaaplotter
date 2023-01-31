533545db2e3c3e28100410fdb059b91a357d8a33
initze <ingmarnitze@gmail.com>
2023-01-31 14:43:20 +0100

    refactpring download script

--- noaaplotter/utils.py
+++ noaaplotter/utils.py
@@ -10,8 +10,15 @@
 ########################
 import pandas as pd
 import datetime as dt
-from datetime import timedelta
+from datetime import timedelta, datetime
 import requests, json
+import os
+import numpy as np
+import pandas as pd
+import tqdm
+from joblib import delayed, Parallel
+import csv
+#import datetime
 
 
 def parse_dates(date):
@@ -54,12 +61,6 @@ def parse_dates_YM(date):
         raise('Wrong date format. Either use native datetime format or "YYYY-mm-dd"')
 
 
-def download_from_noaa(station_id, token):
-
-    pass
-    #return df
-
-
 def dl_noaa_api(i, dtypes_string, station_id, Token, date_start, date_end, split_size):
     """
     function to download from NOAA API
@@ -88,3 +89,49 @@ def dl_noaa_api(i, dtypes_string, station_id, Token, date_start, date_end, split
     except:
         result = None
     return result
+
+
+def download_from_noaa(output_file, start_date, end_date, datatypes, loc_name, station_id, noaa_api_token):
+    # remove file if exists
+    if os.path.exists(output_file):
+        os.remove(output_file)
+    # Make query string
+    dtypes_string = '&'.join([f'datatypeid={dt}' for dt in datatypes])
+    # convert datestring to dt
+    dt_start = datetime.strptime(start_date, '%Y-%m-%d')
+    dt_end = datetime.strptime(end_date, '%Y-%m-%d')
+    # calculate number of days
+    n_days = (dt_end - dt_start).days
+    # calculate number of splits to fit into 1000 lines/rows
+    split_size = np.floor(1000 / len(datatypes))
+    # calculate splits
+    split_range = np.arange(0, n_days, split_size)
+    # Data Loading
+    print('Downloading data through NOAA API')
+    datasets_list = Parallel(n_jobs=4)(
+        delayed(dl_noaa_api)(i, dtypes_string, station_id, noaa_api_token, start_date, end_date, split_size)
+        for i in tqdm.tqdm(split_range[:])
+    )
+    # Merge subsets and create DataFrame
+    df = pd.concat(datasets_list)
+    #### Pivot table to correct form
+    df_pivot = df.pivot(index='date', columns='datatype', values='value')
+    #### adapt  factor
+    df_pivot.loc[:, :] /= 10
+    df_pivot = df_pivot.reset_index(drop=False)
+    df_pivot['DATE'] = df_pivot.apply(lambda x: datetime.fromisoformat(x['date']).strftime('%Y-%m-%d'), axis=1)
+    dr = pd.DataFrame(pd.date_range(start=start_date, end=end_date), columns=['DATE'])
+    dr['DATE'] = dr['DATE'].astype(str)
+    df_merged = pd.concat([df_pivot.set_index('DATE'), dr.set_index('DATE')], join='outer', axis=1,
+                          sort=True)
+    df_merged['DATE'] = df_merged.index
+    df_merged['STATION'] = station_id
+    df_merged['NAME'] = loc_name
+    df_merged['TAVG'] = None
+    df_merged['SNWD'] = None
+    final_cols = ["STATION", "NAME", "DATE", "PRCP", "SNWD", "TAVG", "TMAX", "TMIN"]
+    df_final = df_merged[final_cols]
+    df_final = df_final.replace({np.nan: None})
+    print(f'Saving data to {output_file}')
+    df_final.to_csv(output_file, index=False, quoting=csv.QUOTE_ALL)
+    return 0
--- scripts/download_data.py
+++ scripts/download_data.py
@@ -9,7 +9,7 @@ import os
 import pandas as pd
 import tqdm
 from joblib import delayed, Parallel
-from noaaplotter.utils import dl_noaa_api
+from noaaplotter.utils import dl_noaa_api, download_from_noaa
 
 
 def main():
@@ -46,53 +46,7 @@ def main():
 
     args = parser.parse_args()
 
-    download_function(args.output_file, args.start_date, args.end_date, args.datatypes, args.loc_name, args.station_id, args.token)
-
-
-def download_function(output_file, start_date, end_date, datatypes, loc_name, station_id, noaa_api_token):
-    # remove file if exists
-    if os.path.exists(output_file):
-        os.remove(output_file)
-    # Make query string
-    dtypes_string = '&'.join([f'datatypeid={dt}' for dt in datatypes])
-    # convert datestring to dt
-    dt_start = datetime.strptime(start_date, '%Y-%m-%d')
-    dt_end = datetime.strptime(end_date, '%Y-%m-%d')
-    # calculate number of days
-    n_days = (dt_end - dt_start).days
-    # calculate number of splits to fit into 1000 lines/rows
-    split_size = np.floor(1000 / len(datatypes))
-    # calculate splits
-    split_range = np.arange(0, n_days, split_size)
-    # Data Loading
-    print('Downloading data through NOAA API')
-    datasets_list = Parallel(n_jobs=4)(
-        delayed(dl_noaa_api)(i, dtypes_string, station_id, noaa_api_token, start_date, end_date, split_size)
-        for i in tqdm.tqdm(split_range[:])
-    )
-    # Merge subsets and create DataFrame
-    df = pd.concat(datasets_list)
-    #### Pivot table to correct form
-    df_pivot = df.pivot(index='date', columns='datatype', values='value')
-    #### adapt  factor
-    df_pivot.loc[:, :] /= 10
-    df_pivot = df_pivot.reset_index(drop=False)
-    df_pivot['DATE'] = df_pivot.apply(lambda x: datetime.fromisoformat(x['date']).strftime('%Y-%m-%d'), axis=1)
-    dr = pd.DataFrame(pd.date_range(start=start_date, end=end_date), columns=['DATE'])
-    dr['DATE'] = dr['DATE'].astype(str)
-    df_merged = pd.concat([df_pivot.set_index('DATE'), dr.set_index('DATE')], join='outer', axis=1,
-                          sort=True)
-    df_merged['DATE'] = df_merged.index
-    df_merged['STATION'] = station_id
-    df_merged['NAME'] = loc_name
-    df_merged['TAVG'] = None
-    df_merged['SNWD'] = None
-    final_cols = ["STATION", "NAME", "DATE", "PRCP", "SNWD", "TAVG", "TMAX", "TMIN"]
-    df_final = df_merged[final_cols]
-    df_final = df_final.replace({np.nan: None})
-    print(f'Saving data to {output_file}')
-    df_final.to_csv(output_file, index=False, quoting=csv.QUOTE_ALL)
-    return 0
+    download_from_noaa(args.output_file, args.start_date, args.end_date, args.datatypes, args.loc_name, args.station_id, args.token)
 
 
 if __name__ == "__main__":
