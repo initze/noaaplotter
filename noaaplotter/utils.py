1cc3c0196098bd12b60dcd6bfaf2b696623780dc
Ingmar Nitze <ingmarnitze@gmail.com>
2023-02-18 18:05:40 +0100

    some restructuring of scripts

--- noaaplotter/utils.py
+++ noaaplotter/utils.py
@@ -8,16 +8,12 @@
 # version: 2020-12-09
 
 ########################
-import pandas as pd
 import datetime as dt
-from datetime import timedelta, datetime
+from datetime import timedelta
 import requests, json
-import os
-import numpy as np
 import pandas as pd
-import tqdm
-from joblib import delayed, Parallel
-import csv
+
+
 #import datetime
 
 
@@ -101,50 +97,6 @@ def dl_noaa_api(i, dtypes, station_id, Token, date_start, date_end, split_size):
     except:
         result = None
     return result
-
-
-def download_from_noaa(output_file, start_date, end_date, datatypes, loc_name, station_id, noaa_api_token):
-    # remove file if exists
-    if os.path.exists(output_file):
-        os.remove(output_file)
-    # Make query string
-    dtypes_string = '&'.join([f'datatypeid={dt}' for dt in datatypes])
-    # convert datestring to dt
-    dt_start = datetime.strptime(start_date, '%Y-%m-%d')
-    dt_end = datetime.strptime(end_date, '%Y-%m-%d')
-    # calculate number of days
-    n_days = (dt_end - dt_start).days
-    # calculate number of splits to fit into 1000 lines/rows
-    split_size = np.floor(1000 / len(datatypes))
-    # calculate splits
-    split_range = np.arange(0, n_days, split_size)
-    # Data Loading
-    print('Downloading data through NOAA API')
-    datasets_list = Parallel(n_jobs=4)(
-        delayed(dl_noaa_api)(i, datatypes, station_id, noaa_api_token, start_date, end_date, split_size)
-        for i in tqdm.tqdm(split_range[:])
-    )
-    # Merge subsets and create DataFrame
-    df = pd.concat(datasets_list)
-
-    df_pivot = assign_numeric_datatypes(df)
-    df_pivot['DATE'] = df_pivot.apply(lambda x: datetime.fromisoformat(x['DATE']).strftime('%Y-%m-%d'), axis=1)
-
-    df_pivot = df_pivot.reset_index(drop=False)
-    dr = pd.DataFrame(pd.date_range(start=start_date, end=end_date), columns=['DATE'])
-    dr['DATE'] = dr['DATE'].astype(str)
-    df_merged = pd.concat([df_pivot.set_index('DATE'), dr.set_index('DATE')], join='outer', axis=1,
-                          sort=True)
-    df_merged['DATE'] = df_merged.index
-    df_merged['NAME'] = loc_name
-    df_merged['TAVG'] = None
-    df_merged['SNWD'] = None
-    final_cols = ["STATION", "NAME", "DATE", "PRCP", "SNWD", "TAVG", "TMAX", "TMIN"]
-    df_final = df_merged[final_cols]
-    df_final = df_final.replace({np.nan: None})
-    print(f'Saving data to {output_file}')
-    df_final.to_csv(output_file, index=False, quoting=csv.QUOTE_ALL)
-    return 0
 
 
 def assign_numeric_datatypes(df):
